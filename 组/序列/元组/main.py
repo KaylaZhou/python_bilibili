777ef65b25a3b180ed0576109c3b195cf59d5661
Kayla <809246049@qq.com>
2020-01-20 00:43:11 +0800

    学习更新

---
+++ 组/序列/元组/main.py
@@ -0,0 +1,14 @@
+print((1, 2, 3, 4, 5,))
+print((1, 2, 3) + (4, 5, 6))
+print((1, 2, 3) * 2)
+print((1, 2, 3, 4, 5)[3])
+print((1, 2, 3, 4, 5)[0:3])
+print((-1, '3', 'kayla', 'True', '3.8', ['北京'], {'你好'}))
+
+
+print(type((1, 2, 3, 4, 5)))
+# <class 'tuple'>
+print(type((2)))  # 系统会将()识别为数学运算符类处理
+# <class 'int' >
+print(type((2,)))  # 加个逗号后,会识别为元组
+# <class 'tuple' >
