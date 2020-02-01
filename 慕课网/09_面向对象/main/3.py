bbaa77b4904ac712b1228f5ba644e9b1b9cd431a
Kayla <809246049@qq.com>
2020-02-01 19:43:14 +0800

    学习更新

---
+++ 慕课网/09_面向对象/main/3.py
@@ -0,0 +1,13 @@
+class Student():
+   # 类变量
+    name = ''
+    age = 0
+
+    def __init__(self):
+        self.name = name
+        age = age
+        print('student')
+
+
+student1 = Student()
+print()
