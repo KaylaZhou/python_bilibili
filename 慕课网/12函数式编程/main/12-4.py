f5f8e5575b928f8767483be5e65ee690e8e7d448
Kayla <809246049@qq.com>
2020-02-21 02:50:02 +0800

    提交

---
+++ 慕课网/12函数式编程/main/12-4.py
@@ -0,0 +1,14 @@
+
+def c():
+
+    list_x = [1, 0, 0, 1, 0, 1]
+    # 过滤剔除掉0
+    r = filter(lambda x: True if x == 1 else False, list_x)
+    r1 = filter(lambda x: x, list_x)
+
+    # 与map一样, 最终得到的都是一个集合, 所以打印时需要list转换一下
+    print(list(r))
+    print(list(r1))
+
+
+# c()
