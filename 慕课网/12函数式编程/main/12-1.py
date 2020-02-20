e3bc855b340b2137ae4289054006a114100f33b2
Kayla <809246049@qq.com>
2020-02-20 23:38:28 +0800

    更新

--- 慕课网/12函数式编程/main/12-1.py
+++ 慕课网/12函数式编程/main/12-1.py
@@ -1,4 +1,25 @@
-def g(x): return x * 2
+# 常规函数
+def add(x, y):
+    return x+y
 
 
-print(g(3))
+add(1, 2)
+
+# 匿名函数
+f=lambda x, y: x + y
+
+print(f(1, 2))
+
+
+# 三元表达式
+# 例子: x>y 取x,否则 取y
+
+# 其他语言的三元表达式
+x > y ? x : y  # ?前面是条件判断,返回不同的结果用冒号相连(如果x>y,返回x,否则返回y)
+
+# python 中的三元表达
+# 条件为真时返回的结果if 条件判断else 条件为假时的返回结果
+x=2
+y=5
+r=x if x > y else y
+print(r)
