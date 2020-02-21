adfe64e0481d8fa5b88468fd0cbcef2ad6e7a33f
Kayla <809246049@qq.com>
2020-02-22 01:05:38 +0800

    更新提交

--- 慕课网/12函数式编程/main/12-5_2.py
+++ 慕课网/12函数式编程/main/12-5_2.py
@@ -1,2 +1,17 @@
-def decorator():
-    pass
+# 装饰器
+import time
+
+
+def decorator(func):
+    def wrapper():
+        print(time.time())
+        func()
+    return wrapper
+
+
+@decorator
+def f1():
+    print('This is a function')
+
+
+f1()  # 不需要改变原来的调用方式
