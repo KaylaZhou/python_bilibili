a2439c6c750a403f582480ff1cfdfd98f6168f60
Kayla <809246049@qq.com>
2020-03-02 17:28:41 +0800

    更新

---
+++ Py_库/jquery.md
@@ -0,0 +1,13 @@
+# python 库
+
+## jquery
+
+### 安装 jquery 库
+
+    pip install pyquery
+
+### 检查 jquery 库
+
+    In [1]: import pyquery
+    In [2]: pyquery.text
+    Out[2]: <module 'pyquery.text' from '/root/pp1/.venv/lib/python3.6/site-packages/pyquery/   text.py'>
--- 慕课网/13 原生爬虫/main/13-1.py
+++ 慕课网/13 原生爬虫/main/13-1.py
@@ -1,15 +1,3 @@
-from urllib import request
-# 断点调试
+#
 
-
-class Spider():
-    def __fetch_content(self):
-        r = request.urlopen(Spider.url)
-        htmls = r.read()
-
-    def go(self):
-        self.__fetch_content()
-
-
-spider = Spider()
-spider.go()
+import pyquery
