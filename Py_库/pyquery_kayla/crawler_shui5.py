7a3251d67b2fedbe34f8bac504e6e560a2f4b4b7
Kayla <809246049@qq.com>
2020-03-04 18:23:14 +0800

    提交

--- Py_库/pyquery_kayla/crawler_shui5.py
+++ Py_库/pyquery_kayla/crawler_shui5.py
@@ -1,14 +1,22 @@
 from urllib import request
 
-reqUrl = "http://www.jianshu.com"
+# 通过向Request对象发送请求,获取网址
+req = request.Request('http://www.shui5.cn/')
 
-req = request.Request(reqUrl)
+# 使用add_header()方法添加信息 查看网址代码中network下的name下的headers中最后部分User-Agent:
 req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36 Edg/80.0.361.62')
 
+# 通过urlopen函数,获取网址数据
 resp = request.urlopen(req)
 
+# 读取文件
 html = resp.read()
 
-writeTxt = html.decode('utf-8')
+# decode中的内容查看爬取网页代码中network下的name下的charset="xxx"
+writeTxt = html.decode('gb2312')
 
-# 写文件
+
+file = open('./shui5.html', "w", encoding='gb2312')
+file.write(writeTxt)
+
+print(writeTxt)
--- 慕课网/13 原生爬虫/main/crawler_mark.py
+++ 慕课网/13 原生爬虫/main/crawler_mark.py
@@ -4,6 +4,7 @@ from urllib import request
 
 req = request.Request('http://www.jianshu.com')
 req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36 Edg/80.0.361.62')
+
 resp = request.urlopen(req)
 html = resp.read()
 
